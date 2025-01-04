164c398d159a0701e2dddd10788851ae24e4787b
qqueiros <quentin.queiros@gmail.com>
2025-01-04 15:35:27 +0100

    complete train code

--- code/model/train.py
+++ code/model/train.py
@@ -3,20 +3,23 @@
 # Importing the required libraries
 from roboflow import Roboflow
 from ultralytics import YOLO
-frim clearml import Task
+from clearml import Task
 
 # Creating an instance of the Roboflow class
-rf = Roboflow(api_key="2Z8LedwxqBlKAbVYyz8T")
-project = rf.workspace("ai-course-2024").project("eiders2")
-version = project.version(1)
-dataset = version.download("yolov11")
+#rf = Roboflow(api_key="2Z8LedwxqBlKAbVYyz8T")
+#project = rf.workspace("ai-course-2024").project("eiders2")
+#version = project.version(1)
+#dataset = version.download("yolov11")
                 
 
 # Initialize ClearML task
 task = Task.init(project_name="YOLOv11 Training Eiders", task_name="YOLOv11 Model Eiders")
 
+# Load a COCO-pretrained YOLO11n model
+model = YOLO("models/yolo11m.pt")
+
 # Train the model on the dataset for 50 epochs
-results = model.train(data=dataset, epochs=50, imgsz=1280)
+results = model.train(data="Eiders2-1/data.yaml", epochs=50, imgsz=1280, device = [1])
 
 # Log the results to ClearM
 task.upload_artifact('training_results_dataset_v1', results)
