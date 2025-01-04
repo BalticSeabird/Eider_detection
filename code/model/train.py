fe77ea2f61303793ffef9ba7a7f2a9806abad912
YourName <jonas.sundberg@slu.se>
2025-01-04 14:58:46 +0100

    Training code incl link to clearml

--- code/model/train.py
+++ code/model/train.py
@@ -2,6 +2,8 @@
 
 # Importing the required libraries
 from roboflow import Roboflow
+from ultralytics import YOLO
+frim clearml import Task
 
 # Creating an instance of the Roboflow class
 rf = Roboflow(api_key="2Z8LedwxqBlKAbVYyz8T")
@@ -10,4 +12,17 @@ version = project.version(1)
 dataset = version.download("yolov11")
                 
 
+# Initialize ClearML task
+task = Task.init(project_name="YOLOv11 Training Eiders", task_name="YOLOv11 Model Eiders")
 
+# Train the model on the dataset for 50 epochs
+results = model.train(data=dataset, epochs=50, imgsz=1280)
+
+# Log the results to ClearM
+task.upload_artifact('training_results_dataset_v1', results)
+
+# Save the model
+model.save('models/eider_model_v01.pt')
+
+# Close the ClearML task
+task.close()
