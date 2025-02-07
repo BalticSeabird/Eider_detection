9d62b56912066b43c39436fe78d8cf268535c008
YourName <jonas.sundberg@slu.se>
2025-02-07 21:16:13 +0100

    script for copying inference files

---
+++ code/file_handling/move_grouped_files.py
@@ -0,0 +1,16 @@
+
+from pathlib import Path
+import os
+
+# Define input and output folders
+input_folder = Path("../../../../../../mnt/BSP_NAS2_work/eider_model/inference")
+output_folder = Path("../../../../../../mnt/BSP_NAS2_work/eider_model/temp")
+
+files = list(input_folder.rglob("*grouped.csv"))
+
+# Loop through all files in the input folder
+for file in files: 
+    # Copy to the output folder
+    outname = output_dir.joinpath(file.name)
+    shutil.copy(file, outname)
+    print(f"File {file} copied to {outname}")
