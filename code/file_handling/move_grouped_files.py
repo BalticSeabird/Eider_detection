bee6c9d88b86867801d0c725128f9d75f946478e
qqueiros <quentin.queiros@gmail.com>
2025-02-10 09:45:32 +0100

    Sync files remote

--- code/file_handling/move_grouped_files.py
+++ code/file_handling/move_grouped_files.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+import shutil
 
 # Define input and output folders
 input_folder = Path("../../../../../../mnt/BSP_NAS2_work/eider_model/inference")
@@ -11,6 +12,6 @@ files = list(input_folder.rglob("*grouped.csv"))
 # Loop through all files in the input folder
 for file in files: 
     # Copy to the output folder
-    outname = output_dir.joinpath(file.name)
+    outname = output_folder.joinpath(file.name)
     shutil.copy(file, outname)
     print(f"File {file} copied to {outname}")
