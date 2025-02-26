dbe432ed07cd2be6dbc7eaff798d627499815c69
qqueiros <quentin.queiros@gmail.com>
2025-02-26 16:34:50 +0100

    Create grouped output with confidence

--- code/postprocess/compile_detections.py
+++ code/postprocess/compile_detections.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 
 # Read folders 
-data_path = Path("data/grouped/")
-files = list(data_path.rglob("*.csv"))
+data_path = Path("../../../../../../mnt/BSP_NAS2_work/eider_model/inference/eider_model_nano_v5852/grouped")
+files = list(data_path.rglob("*grouped.csv"))
 
 # Compile data files
 out = pd.DataFrame()
@@ -16,17 +16,17 @@ for file in files:
     out = pd.concat([out, temp])
     print(file.name)
 
-out = out[["datetime", "class", "counts", "station"]]
+out = out[["datetime", "class", "conf", "frame", "station"]]
 
 d = {"name": ['crow', 'eider_female', 'eider_male', 'gull', 'razorbill'], 
     "class": [0, 1, 2, 3, 4]}
 class_id = pd.DataFrame(d)    
 
 out = out.merge(class_id, on = "class")
-out["counts"] = out["counts"].astype("int")
+out["counts"] = out["frame"].astype("int")
 
 out["datetime"] = pd.to_datetime(out["datetime"], format = "mixed")
 
 out.sort_values(by = ["station", "datetime"], inplace = True) 
 
-out.to_csv("data/compiled2sV3.csv", index = False)
+out.to_csv("data/compiled_nanov5852_v2.csv", index = False)
