5b4c2fb5089df24a981328d30c89fa515e444fdd
qqueiros <quentin.queiros@gmail.com>
2025-03-26 16:50:29 +0100

    Update paths

--- code/model/run_inference.py
+++ code/model/run_inference.py
@@ -7,12 +7,13 @@ import sys
 # Input arguments (run device and station)
 device = sys.argv[1]
 stat = sys.argv[2]
+datelimit = pd.to_datetime("2024-05-19")
 
 # Load a pretrained YOLO model
 modelpath = Path("models/eider_model_nano_v5852.pt")
 model = YOLO(modelpath)
 modelname = modelpath.stem
-output_dir1 = f'../../../../../../mnt/BSP_NAS2_work/eider_model/inference/2023/{modelname}/'
+output_dir1 = f'../../../../../../mnt/BSP_NAS2_work/eider_model/inference/2024/{modelname}/'
 
 if os.path.exists(output_dir1) == False:
     os.makedirs(output_dir1)
@@ -24,98 +25,110 @@ if os.path.exists(output_dir2) == False:
     os.makedirs(output_dir2)
 
 # Define input video path
-base_path = Path(f"../../../../../../mnt/BSP_NAS2/Video/Video2023/{stat}")
+base_path = Path(f"../../../../../../mnt/BSP_NAS2_vol3/Video/Video2024/{stat}")
 vids = list(base_path.rglob("*.mp4"))
 vids.sort()
 
+
+# Remove strange files with ""eadir" in the name
+vids = [vid for vid in vids if "@eaDir" not in str(vid)]
+
+
 for vid in vids: 
     filename = vid.name
+    datevid = pd.to_datetime(vid.parents[0].name)
 
-    # Pick out relevant information from name
-    name = filename.split("_")
-    time = name[-2]+" "+name[-1][0:8]
-    time = time.replace(".", ":")
-    station = stat
+    if datevid > pd.to_datetime(datelimit):
 
-    try:
+        # Pick out relevant information from name
+        name = filename.split("_")
+        time = name[-2]+" "+name[-1][0:8]
+        time = time.replace(".", ":")
+        station = stat
 
-        starttime = pd.to_datetime(time)
-        starttime_u = starttime.timestamp()
-        fps = 25
+        try:
 
-        outname = output_dir2+"/"+vid.stem+"_raw.csv"
-        outname_grouped = output_dir2+"/"+vid.stem+"_grouped.csv"
+            starttime = pd.to_datetime(time)
+            starttime_u = starttime.timestamp()
+            fps = 25
 
-        # Check that file has not been processed already 
+            outname = output_dir2+"/"+vid.stem+"_raw.csv"
+            outname_grouped = output_dir2+"/"+vid.stem+"_grouped.csv"
 
-        if os.path.exists(outname):
-            print(f"File {outname} already exists")
-            continue
+            # Check that file has not been processed already 
 
-        else: 
-            # Run inference using the pretrained model and the inout video
-            results = model.predict(vid, 
-                                stream=True, 
-                                save = False,
-                                show = False, 
-                                device = device)
+            if os.path.exists(outname):
+                print(f"File {outname} already exists")
+                continue
 
-            # Process results list
-            time = []
-            boxes = []
-            #track_ids = []
-            confs = []
-            classes = []
-            framenum = []
-            counter = starttime_u
-            counterx = 0
+            else: 
+                # Run inference using the pretrained model and the inoutvid video
+                results = model.predict(vid, 
+                                    stream=True, 
+                                    save = False,
+                                    show = False, 
+                                    device = device)
 
-            for r in results:
-                if not r.boxes.conf.nelement() == 0 :
-                    boxes.append(r.boxes.xyxy.tolist())
-                    #if r.boxes.id is not None:
-                    #    track_ids.append(r.boxes.id.tolist())
-                    #else:
-                    #    track_ids.append([-1 for _ in r.boxes])
-                    classes.append(r.boxes.cls.tolist())
-                    confs.append(r.boxes.conf.tolist())
-                    ndetect = len(r.boxes.conf.tolist())
-                    time.append([counter] * ndetect)
-                    framenum.append([counterx] * ndetect)
+                # Process results list
+                time = []
+                boxes = []
+                #track_ids = []
+                confs = []
+                classes = []
+                framenum = []
+                counter = starttime_u
+                counterx = 0
 
-                counter += 1/fps
-                counterx += 1
+                for r in results:
+                    if not r.boxes.conf.nelement() == 0 :
+                        boxes.append(r.boxes.xyxy.tolist())
+                        #if r.boxes.id is not None:
+                        #    track_ids.append(r.boxes.id.tolist())
+                        #else:
+                        #    track_ids.append([-1 for _ in r.boxes])
+                        classes.append(r.boxes.cls.tolist())
+                        confs.append(r.boxes.conf.tolist())
+                        ndetect = len(r.boxes.conf.tolist())
+                        time.append([counter] * ndetect)
+                        framenum.append([counterx] * ndetect)
 
-            # Concatenate outputs
-            conf = sum(confs, [])
-            classes = sum(classes, [])
-            boxesx = sum(boxes, [])
-            #track_ids = sum(track_ids, [])
-            times = sum(time, [])
-            framenums = sum(framenum, [])
+                    counter += 1/fps
+                    counterx += 1
 
-            # Save as data frames
-            #out1 = pd.DataFrame(boxesx, columns = ["x", "y", "w", "h"])
-            out2 = pd.DataFrame(list(zip(classes, conf, times, framenums)), columns = ["class", "conf", "time", "frame"])
+                # Concatenate outputs
+                conf = sum(confs, [])
+                classes = sum(classes, [])
+                boxesx = sum(boxes, [])
+                #track_ids = sum(track_ids, [])
+                times = sum(time, [])
+                framenums = sum(framenum, [])
 
-            #out = out1.merge(out2, left_index = True, right_index = True)
-            out2["station"] = station
-            out2["filename"] = filename
+                # Save as data frames
+                #out1 = pd.DataFrame(boxesx, columns = ["x", "y", "w", "h"])
+                out2 = pd.DataFrame(list(zip(classes, conf, times, framenums)), columns = ["class", "conf", "time", "frame"])
 
-            # Actual time
-            out2["datetime"] = pd.to_datetime(out2["time"], unit = "s")
+                #out = out1.merge(out2, left_index = True, right_index = True)
+                out2["station"] = station
+                out2["filename"] = filename
 
-            # Group by second
-            grouped_data = out2.groupby([pd.Grouper(key='datetime', freq='2s'), "class"])
+                # Actual time
+                out2["datetime"] = pd.to_datetime(out2["time"], unit = "s")
 
-            # Aggregate grouped_data (mean confidence score)
-            grouped = grouped_data.agg({"conf": "mean", 
-                            "frame": "count"}).reset_index()        
+                # Group by second
+                grouped_data = out2.groupby([pd.Grouper(key='datetime', freq='2s'), "class"])
 
-            out2.to_csv(outname, index = False)
-            grouped.to_csv(outname_grouped, index = False)
-    except: 
-       print(f"Error with {vid} (probably file name), continue loop")
+                # Aggregate grouped_data (mean confidence score)
+                grouped = grouped_data.agg({"conf": "mean", 
+                                "frame": "count"}).reset_index()        
+
+                out2.to_csv(outname, index = False)
+                grouped.to_csv(outname_grouped, index = False)
+        except: 
+            print(f"Error with {vid} (probably file name), continue loop")
+    else:   
+        continue
+
+
 
 # Run example 
 # python3 code/model/run_inference.py 1 "EJDER4"
--- code/postprocess/compile_detections.py
+++ code/postprocess/compile_detections.py
@@ -11,10 +11,10 @@ files = list(data_path.rglob("*grouped5s.csv"))
 out = pd.DataFrame()
 for file in files:
     temp = pd.read_csv(file)
-    temp["station"] = file.name.split("_")[-4]
+    temp["station"] = file.name.split("_")[-5]
     temp.rename(columns = {"datetime": "datetime", "class": "class", "conf": "conf", "frame": "counts"}, inplace = True)
     out = pd.concat([out, temp])
-    print(file.name)
+    print(f'compiling ... {file.name}')
 
 out = out[["datetime", "class", "conf", "counts", "station"]]
 
@@ -29,3 +29,5 @@ out["datetime"] = pd.to_datetime(out["datetime"], format = "mixed")
 out.sort_values(by = ["station", "datetime"], inplace = True) 
 
 out.to_csv("../../../../../../mnt/BSP_NAS2_work/eider_model/inference/eider2024_nanov5852_v5.csv", index = False)
+
+print(f'example rows: {out.head()}')
