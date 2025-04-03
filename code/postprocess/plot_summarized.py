f23c9a1a0fe82d502701a94e080160de12404707
YourName <jonas.sundberg@slu.se>
2025-04-03 16:49:55 +0200

    Updated plotting script

--- code/postprocess/plot_summarized.py
+++ code/postprocess/plot_summarized.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import sys 
 
-# Plot time series of one station at the time, with datetime as time series and bars of bars for counts
-fig, ax = plt.subplots(5, 1)
 
-# Read data
-out = pd.read_csv("data/eider2024_nanov5852_v5.csv")
-
-# Subset data for one station at the time: 
+# Read data for one station at the time: 
 stat = sys.argv[1]
+out = pd.read_csv(f"data/eider2024_nanov5852_{stat}_v9.csv")
 out = out[out["station"] == stat]
 out["datetime"] = pd.to_datetime(out["datetime"])
 
 
+# Full date time sequence
+date_rng = pd.date_range(start=out["datetime"].min(), end=out["datetime"].max(), freq='5s')
+
+
+# Plot time series of one station at the time, with datetime as time series and bars of bars for counts
+fig, ax = plt.subplots(5, 1)
 
 plt.suptitle(stat)
 
@@ -24,16 +26,13 @@ for i in range(0, 5):
     dx2 = dx2.drop_duplicates(subset='datetime')
 
     # Subset based on confidence level
-    #dx2 = dx2[dx2["conf"] > 0.3]
-
-    # Full date time sequence
-    date_rng = pd.date_range(start=dx2["datetime"].min(), end=dx2["datetime"].max(), freq='2s')
+    dx2 = dx2[dx2["conf"] > 0.6]
 
     # Fill missing values
     dx2 = dx2.set_index('datetime').reindex(date_rng, fill_value=0).reset_index().rename(columns={'index': 'datetime'})
 
     # Create a new y series which is a 10 point running mean of the original y series
-    y = dx2["counts"]/50
+    y = dx2["counts"]/125
     y_rolling = y.rolling(window=10).mean()
 
     ax[i].plot(dx2["datetime"], y, c = "red", alpha = 0.4)
@@ -46,32 +45,3 @@ for i in range(0, 5):
         fontsize='medium', verticalalignment='top', fontfamily='serif',
         bbox=dict(facecolor='0.7', edgecolor='none', pad=3.0))
 plt.show()
-
-""" 
-# Alternative
-fig, ax = plt.subplots(1, 1)
-
-# Subset data for one station at the time: 
-dx = out[out["station"] == stat]
-plt.suptitle(stat)
-cols = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"]
-xs = [0, .2, .4, .6, .8]
-for i in range(0, 5):
-    dx2 = dx[dx["class"] == (i)]
-    ax.plot(dx2["datetime"], dx2["counts"]/50, color = cols[i]) 
-    label = dx2["name"].iloc[0]
-    ax.annotate(label,
-        xy=(xs[i], 1), xycoords='axes fraction', color = cols[i],
-        xytext=(+0.5, -0.5), textcoords='offset fontsize',
-        fontsize='medium', verticalalignment='top', fontfamily='serif',
-        bbox=dict(facecolor='0.7', edgecolor='none', pad=3.0))
-plt.show()
- """
-
-
-
-cond1 = out["station"] == stat
-cond2 = out["datetime"] > "2024-05-03 04:00:00"
-cond3 = out["datetime"] < "2024-05-03 05:00:00"
- 
-look = out[cond1 & cond2 & cond3]
