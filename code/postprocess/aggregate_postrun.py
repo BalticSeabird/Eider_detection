a2929c62834029a3d37058a124501821e4547a30
qqueiros <quentin.queiros@gmail.com>
2025-04-22 21:47:06 +0200

    Minor updates to run code

--- code/postprocess/aggregate_postrun.py
+++ code/postprocess/aggregate_postrun.py
@@ -13,26 +13,27 @@ for file in files:
     outname = file.parent.joinpath(f'{file.stem[0:-3]}grouped5s.csv')
 
     # Check if the file already exists
-    #if outname.exists():
-    #    print(f"File {outname} already exists")
-    #    continue
-    #else: 
-    out2 = pd.read_csv(file, parse_dates = ["datetime"])
-    
-    # Add to the if loop to skip if the file does not exist!
-
-    if len(out2) == 0:
+    if outname.exists():
+        print(f"File {outname} already exists")
         continue
-
     else: 
-        print(f'processing... {file.stem}')
-        grouped_data = out2.groupby([pd.Grouper(key='datetime', freq='5s'), "class"])
+        out2 = pd.read_csv(file, parse_dates = ["datetime"])
+        
+        # Add to the if loop to skip if the file does not exist!
 
-        # Aggregate grouped_data (mean confidence score)
-        grouped = grouped_data.agg({"conf": "mean", 
-                        "frame": "count"}).reset_index()        
+        if len(out2) == 0:
+            print(f"File {outname} empty ... continues")
+            continue
 
-        grouped.to_csv(outname, index = False)
+        else: 
+            print(f'processing ... {file.stem}')
+            grouped_data = out2.groupby([pd.Grouper(key='datetime', freq='5s'), "class"])
+
+            # Aggregate grouped_data (mean confidence score)
+            grouped = grouped_data.agg({"conf": "mean", 
+                            "frame": "count"}).reset_index()        
+
+            grouped.to_csv(outname, index = False)
 
 # Run example
 # python3 code/postprocess/aggregate_postrun.py "EJDER5"
